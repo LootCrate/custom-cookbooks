0994c14485950c70ae4a86d66590bc7a462796bf
Jason Carter <jccarte@gmail.com>
2015-08-14 11:16:12 -0400

    Metadata

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'lootcrate-cookbook'
+maintainer       'Jason Carter'
+maintainer_email 'jason.carter@lootcrate.com'
+license          'All rights reserved'
+description      'Installs/Configures cookbook'
+long_description 'Installs/Configures cookbook'
+version          '0.1.0'
