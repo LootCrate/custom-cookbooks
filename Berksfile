5407397a976d7090421bec1c73de9ecaf26fb669
Jason Carter <jccarte@gmail.com>
2015-08-19 08:37:35 -0400

    trying windows multiple cookbooks again

--- Berksfile
+++ Berksfile
@@ -1,7 +1,5 @@
 source "https://supermarket.chef.io"
 
-metadata
-
 cookbook 'rvm', git: "git://github.com/martinisoft/chef-rvm.git"
 cookbook 'yum'
 cookbook 'rundeck', :path => './rundeck'
--- metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'lootcrate-cookbook'
-maintainer       'Jason Carter'
-maintainer_email 'jason.carter@lootcrate.com'
-license          'All rights reserved'
-description      'Installs/Configures cookbook'
-long_description 'Installs/Configures cookbook'
-version          '0.1.0'
