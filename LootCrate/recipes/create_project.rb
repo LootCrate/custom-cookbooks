6e3e44db62c54660e409bed03eefdaaac5f04976
Jason Carter <jccarte@gmail.com>
2015-08-27 16:23:46 -0400

    Create project

---
+++ LootCrate/recipes/create_project.rb
@@ -0,0 +1,19 @@
+project = node['rundeck-project']
+
+cmd = <<-EOH.to_s
+    rd-project -p #{project} -a create \
+    --resources.source.1.config.file=/var/rundeck/projects/LootCrate/etc/resources.yml \
+    --resources.source.1.config.format=resourceyaml \
+    --resources.source.1.config.generateFileAutomatically=true \
+    --resources.source.1.config.includeServerNode=true \
+    --resources.source.1.config.requireFileExists=false \
+    --resources.source.1.type=file
+    EOH
+
+bash "check-project-#{project}" do
+  user node['rundeck']['user']
+  code cmd
+  not_if do
+    File.exist?("#{node['rundeck']['datadir']}/projects/#{project}/etc/project.properties")
+  end
+end
