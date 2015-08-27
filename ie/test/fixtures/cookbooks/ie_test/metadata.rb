f628904ca9077fe92ea8068a7bb959f592425987
Jason Carter <jccarte@gmail.com>
2015-08-27 14:08:18 -0400

    remove some windows stuff and use node hostname

--- ie/test/fixtures/cookbooks/ie_test/metadata.rb
+++
@@ -1,9 +0,0 @@
-name 'ie_test'
-maintainer 'Dennis Hoer'
-maintainer_email 'dennis.hoer@gmail.com'
-license 'MIT'
-description 'Tests Internet Explorer configuration'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
-
-depends 'ie'
--- ie/test/fixtures/cookbooks/ie_test/recipes/ie_version.rb
+++
@@ -1,1 +0,0 @@
-log ie_version
