9884ddd6ee7d9718568f213b06dadc1e8955c5be
Jason Carter <jccarte@gmail.com>
2015-08-28 13:16:47 -0400

    copy over job files for now

--- LootCrate/recipes/deploy_jobs.rb
+++ LootCrate/recipes/deploy_jobs.rb
@@ -1,3 +1,26 @@
+user = node['rundeck']['user']
+
+directory "/home/#{user}/jobs" do
+  owner user
+  group user
+  mode '0755'
+  action :create
+end
+
+cookbook_file "node['rundeck-jobs-dir']/loot_promo.yml" do
+  source 'loot_promo.yml'
+  owner user
+  group user
+  mode '0644'
+end
+
+cookbook_file "node['rundeck-jobs-dir']/recurly_transactions.yml" do
+  source 'recurly_transactions.yml.yml'
+  owner user
+  group user
+  mode '0644'
+end
+
 bash 'create jobs' do
   user node['rundeck']['user']
   cwd node['rundeck-jobs-dir']
