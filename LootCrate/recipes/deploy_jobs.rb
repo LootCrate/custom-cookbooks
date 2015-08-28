0a7072bbc03cc5b0b8fb04ebb5979309bfe18b57
Jason Carter <jccarte@gmail.com>
2015-08-28 12:51:39 -0400

    fix create jobs

--- LootCrate/recipes/deploy_jobs.rb
+++ LootCrate/recipes/deploy_jobs.rb
@@ -2,6 +2,6 @@ bash 'create jobs' do
   user node['rundeck']['user']
   cwd node['rundeck-jobs-dir']
   code <<-EOH
-  find . -name "*.yml" | xargs rd-jobs load -F yaml --file
+  for i in *.yml; do rd-jobs load -F yaml --file $i; done
   EOH
 end
