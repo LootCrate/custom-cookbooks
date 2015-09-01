5c866abb4d9a0f8eabc9396a9102c1d51706d763
Jason Carter <jccarte@gmail.com>
2015-09-01 16:31:43 -0400

    testing deploy recipe

--- LootCrate/recipes/deploy_scripts.rb
+++ LootCrate/recipes/deploy_scripts.rb
@@ -4,4 +4,16 @@ node[:deploy].each do |application, deploy|
   Chef::Log.info("#{application}")
   Chef::Log.info("#{deploy[:application_type]}")
   Chef::Log.info("deploy to: #{deploy[:deploy_to]}")
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+  
 end
