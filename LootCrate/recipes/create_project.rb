f55a0076b420697d2e1ce544bf930689e0bbdf34
Jason Carter <jccarte@gmail.com>
2015-09-16 11:10:58 -0400

    fix create project

--- LootCrate/recipes/create_project.rb
+++ LootCrate/recipes/create_project.rb
@@ -11,9 +11,9 @@ cmd = <<-EOH.to_s
     EOH
 
 bash "check-project-#{project}" do
-  user node['rundeck']['user']
+  user rundeck
   code cmd
   not_if do
-    File.exist?("#{node['rundeck']['datadir']}/projects/#{project}/etc/project.properties")
+    File.exist?("/var/rundeck/projects/#{project}/etc/project.properties")
   end
 end
