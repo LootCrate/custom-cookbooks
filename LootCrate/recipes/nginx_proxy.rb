47055b23e16f9052a16271f41ca6faf800a08099
Jason Carter <jccarte@gmail.com>
2015-09-16 10:47:00 -0400

    reverse proxy

--- LootCrate/recipes/nginx_proxy.rb
+++ LootCrate/recipes/nginx_proxy.rb
@@ -1,3 +1,15 @@
 package 'nginx' do
   action :install
 end
+
+template '/etc/nginx/sites-available/default' do
+  source 'rundeck-nginx.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+service 'nginx' do
+  supports :status => true, :restart => true, :reload => true
+  action [:start, :enable]
+end
