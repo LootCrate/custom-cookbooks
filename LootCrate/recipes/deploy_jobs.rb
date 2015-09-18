3d2a5f5b38b0353e9c8aaccc0c8fd969d57d19fb
Jason Carter <jccarte@gmail.com>
2015-09-18 13:24:39 -0400

    i can't type

--- LootCrate/recipes/deploy_jobs.rb
+++ LootCrate/recipes/deploy_jobs.rb
@@ -23,7 +23,7 @@ end
 
 bash 'create jobs' do
   user node['rundeck']['user']
-  cwd node['rundeck-jobs-dir']
+  cwd node['rundeck']['jobs-dir']
   code <<-EOH
   for i in *.yml; do rd-jobs load -F yaml --file $i; done
   EOH
