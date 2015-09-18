a9cfade49e5879c487c2e5d181d613277223edc3
Jason Carter <jccarte@gmail.com>
2015-09-18 11:26:27 -0400

    python deps installs

--- LootCrate/attributes/default.rb
+++ LootCrate/attributes/default.rb
@@ -1,2 +1,10 @@
 default['rundeck-project'] = 'LootCrate'
 default['rundeck-jobs-dir'] = '/home/rundeck/jobs'
+default['etl_ubuntu_packages'] = ['python-psycopg2',
+                                  'python-setuptools',
+                                  'python-pip',
+                                  'python-dev',
+                                  'libpq-dev'
+                                  ]
+default['etl_pip_packages'] = ['facebookads'
+                              ]
--- LootCrate/recipes/etl_deps.rb
+++ LootCrate/recipes/etl_deps.rb
@@ -1,3 +1,12 @@
-package 'python-psycopg2' do
-  action :install
+node['etl_ubuntu_packages'].each do |pkg|
+    package pkg do
+        :install
+    end
+end
+
+node['etc_pip_packages'].each do |pkg|
+    execute "install-#{pkg}" do
+        command "pip install #{pkg}"
+        #not_if "[ `pip freeze | grep #{pkg} | cut -d'=' -f3` = '#{version}' ]"
+    end
 end
